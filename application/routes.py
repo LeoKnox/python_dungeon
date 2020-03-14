02525e448a939a895d731d8a7b8eeb50f8eec3e3
LeoKnox <LeoKnox@users.noreply.github.com>
2020-03-13 22:34:55 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -4,16 +4,20 @@ from flask import render_template
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html", dungeon="First")
+    roomData = [{"roomID":"1111","roomName":"Entry","material":"Stone","length":5,"width":5},
+        {"roomID":"1112","roomName":"Throne","material":"Gold","length":7,"width":9},
+        {"roomID":"1113","roomName":"Prison","material":"Iron","length":8,"width":6}]
+
+    return render_template("index.html", dungeon="First", roomData = roomData, index=True)
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", login=True)
 
 @app.route("/room")
 def room():
-    return render_template("room.html", dungeon="First")
+    return render_template("room.html", dungeon="First", room=True)
 
 @app.route("/create")
 def create():
-    return render_template("create.html")
+    return render_template("create.html", create=True)

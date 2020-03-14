58e049a3048b17f41b88e43f8d1ccfadc7550737
LeoKnox <LeoKnox@users.noreply.github.com>
2020-03-13 20:34:18 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -8,8 +8,12 @@ def index():
 
 @app.route("/login")
 def login():
-    return render_template("index.html", dungeon="First")
+    return render_template("login.html")
 
 @app.route("/room")
 def room():
-    return render_template("index.html", dungeon="First")
+    return render_template("room.html", dungeon="First")
+
+@app.route("/create")
+def create():
+    return render_template("create.html")
